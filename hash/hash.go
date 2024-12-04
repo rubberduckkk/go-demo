331cb7c4a8ff2c69e9b4308ec385efe0755c9817
luocheng <luocheng@wepie.com>
2024-12-04 10:26:08 +0800

    feat: hash demo

--- hash/hash.go
+++ hash/hash.go
@@ -20,3 +20,16 @@ func CRC32(str []byte) uint32 {
 func CRC64(str []byte) uint64 {
 	return crc64.Checksum(str, crc64.MakeTable(crc64.ISO))
 }
+
+func HashCode(src string, bucketCount uint32) int {
+	var hash uint32 = 0
+	length := len(src)
+	for i := 0; i < length; i++ {
+		if i&1 == 0 {
+			hash ^= (hash << 7) ^ uint32(src[i]) ^ (hash >> 3)
+		} else {
+			hash ^= ^((hash << 11) ^ uint32(src[i]) ^ (hash >> 5))
+		}
+	}
+	return int(hash % bucketCount)
+}
