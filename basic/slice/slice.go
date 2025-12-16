fcc08f40d4c00aae0d1a67294f08f8d2be097c73
Cheng Luo <chengluo.go@gmail.com>
2025-12-16 11:06:21 +0800

    feat: range nil slice

--- basic/slice/slice.go
+++ basic/slice/slice.go
@@ -1,9 +1,19 @@
 package main
 
+import "fmt"
+
 func makeSlice(c int) []int {
 	return make([]int, 0, c)
 }
 
 func main() {
-	makeSlice(-1)
+	//makeSlice(-1)
+	rangeOverNilSlice(nil)
+}
+
+func rangeOverNilSlice(slice []int) {
+	//var slice []int
+	for _, value := range slice {
+		fmt.Println(value)
+	}
 }
